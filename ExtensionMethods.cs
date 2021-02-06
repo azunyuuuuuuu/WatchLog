97fd077f7f8aff62069d37b7ef98b4837d9203ba
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2021-02-06 19:18:19 +0100

    made the switch to cocona

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -5,19 +5,19 @@ namespace WatchLog
 {
     public static class ExtensionMethods
     {
-        public static IEnumerable<string> RemoveQuoteMarks(this IEnumerable<string> input) =>
-            input.Select(x => x.RemoveQuoteMarks());
+        public static IEnumerable<string> RemoveQuoteMarks(this IEnumerable<string> input)
+            => input.Select(x => x.RemoveQuoteMarks());
 
-        public static string RemoveQuoteMarks(this string input) =>
-            input.Length > 2
-            && input.First() == '"'
-            && input.Last() == '"' ? input.Substring(1, input.Length - 2) : input;
+        public static string RemoveQuoteMarks(this string input)
+            => input.Length > 2
+               && input.First() == '"'
+               && input.Last() == '"' ? input.Substring(1, input.Length - 2) : input;
 
-        public static IEnumerable<string> RemoveStartingSlash(this IEnumerable<string> input) =>
-            input.Select(x => x.RemoveStartingSlash());
+        public static IEnumerable<string> RemoveStartingSlash(this IEnumerable<string> input)
+            => input.Select(x => x.RemoveStartingSlash());
 
-        public static string RemoveStartingSlash(this string input) =>
-            input.Length > 1
-            && input.First() == '\\' ? input.Substring(1, input.Length - 1) : input;
+        public static string RemoveStartingSlash(this string input)
+            => input.Length > 1
+               && input.First() == '\\' ? input.Substring(1, input.Length - 1) : input;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using CliFx;
+using CliWrap;
+using CliWrap.Buffered;
+using Cocona;
 
 namespace WatchLog
 {
-    public static class Program
+    public class Program : CoconaLiteConsoleAppBase
     {
-        public static async Task<int> Main() =>
-            await new CliApplicationBuilder()
-                .AddCommandsFromThisAssembly()
-                .Build()
-                .RunAsync();
+        public static async Task Main(string[] args)
+            => await CoconaLiteApp.RunAsync<Program>(args);
+
+        public async Task LogCommand(
+            [Argument] string command,
+            [Argument] string[] arguments,
+            [Option] double delay = 2.0)
+        {
+            var temp = arguments.RemoveQuoteMarks().RemoveStartingSlash();
+
+            Console.WriteLine($"{command} {string.Join(' ', temp)}");
+
+            var buffers = new Buffers(
+                oldbuffer: string.Empty,
+                newbuffer: string.Empty);
+
+            while (!Context.CancellationToken.IsCancellationRequested)
+                buffers = await ExecuteCommandInLoopAsync(command, temp, buffers, delay);
+        }
+
+        private async Task<Buffers> ExecuteCommandInLoopAsync(
+            string command,
+            IEnumerable<string> arguments,
+            Buffers buffers,
+            double delay)
+        {
+            var cli = Cli.Wrap(command);
+
+            if (arguments.Count() > 0)
+                cli = cli.WithArguments(arguments, false);
+
+            var result = await cli.ExecuteBufferedAsync();
+
+            var newbuffers = buffers with
+            {
+                oldbuffer = buffers.newbuffer,
+                newbuffer = result.StandardOutput
+            };
+
+            OutputDifferences(newbuffers);
+
+            try
+            {
+                await Task.Delay(TimeSpan.FromSeconds(delay), Context.CancellationToken);
+            }
+            catch (TaskCanceledException) { }
+
+            return newbuffers;
+        }
+
+        private record Buffers(string oldbuffer, string newbuffer);
+
+        private void OutputDifferences(Buffers buffers)
+        {
+            var timestamp = DateTimeOffset.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var tempnew = buffers.newbuffer.Split(Environment.NewLine);
+            var tempold = buffers.oldbuffer.Split(Environment.NewLine);
+            var tempadded = tempnew.Where(x => !tempold.Contains(x));
+            var tempremoved = tempold.Where(x => !tempnew.Contains(x));
+
+            foreach (var line in tempadded)
+                Console.WriteLine($"{timestamp} \u001b[32m[ADD]\u001b[0m: {line}");
+            foreach (var line in tempremoved)
+                Console.WriteLine($"{timestamp} \u001b[31m[REM]\u001b[0m: {line}");
+        }
+
     }
 }
